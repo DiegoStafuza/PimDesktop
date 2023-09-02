af8ce837a67e0e4952e19b758c379e9b6d49b800
Diego Stafuza <diegostaf@outlook.com>
2023-09-02 17:40:44 -0300

    Tela de login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PimDesktop
+namespace EstudoWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            SizeChanged += MainWindow_SizeChanged; // Adicione o manipulador de eventos SizeChanged
         }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Mantenha a proporção da janela
+            double width = e.NewSize.Width;
+            double height = e.NewSize.Height;
+
+            // Ajuste o tamanho da janela mantendo a proporção desejada (por exemplo, 16:9)
+            double desiredAspectRatio = 16.0 / 9.0;
+
+            if (width / height > desiredAspectRatio)
+            {
+                // Ajuste a altura para corresponder à proporção
+                this.Width = height * desiredAspectRatio;
+            }
+            else
+            {
+                // Ajuste a largura para corresponder à proporção
+                this.Height = width / desiredAspectRatio;
+            }
+        }
+
     }
 }
